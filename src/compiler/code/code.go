fe1955d484566e4f6f11c35a0e5cadeb277a1b72
KhushPatibandha <khush.patibandha@gmail.com>
2025-02-08 21:14:16 +0530

    compiler init, infix added

---
+++ src/compiler/code/code.go
@@ -0,0 +1,137 @@
+package code
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+type (
+	Instructions []byte
+	Opcode       byte
+)
+
+const (
+	OpConstant Opcode = iota
+	OpPop
+	OpTrue
+	OpFalse
+	OpAdd
+	OpSub
+	OpMul
+	OpDiv
+	OpMod
+	OpAnd
+	OpOr
+	OpEqualEqual
+	OpNotEqual
+	OpGreaterThan
+	OpGreaterThanEqual
+	OpAndAnd
+	OpOrOr
+)
+
+type Defination struct {
+	Name          string
+	OperandWidths []int
+}
+
+var definations = map[Opcode]*Defination{
+	OpConstant:         {"OpConstant", []int{2}},
+	OpPop:              {"OpPop", []int{}},
+	OpTrue:             {"OpTrue", []int{}},
+	OpFalse:            {"OpFalse", []int{}},
+	OpAdd:              {"OpAdd", []int{}},
+	OpSub:              {"OpSub", []int{}},
+	OpMul:              {"OpMul", []int{}},
+	OpDiv:              {"OpDiv", []int{}},
+	OpMod:              {"OpMod", []int{}},
+	OpAnd:              {"OpAnd", []int{}},
+	OpOr:               {"OpOr", []int{}},
+	OpEqualEqual:       {"OpEqualEqual", []int{}},
+	OpNotEqual:         {"OpNotEqual", []int{}},
+	OpGreaterThan:      {"OpGreaterThan", []int{}},
+	OpGreaterThanEqual: {"OpGreaterThanEqual", []int{}},
+	OpAndAnd:           {"OpAndAnd", []int{}},
+	OpOrOr:             {"OpOrOr", []int{}},
+}
+
+func Lookup(op byte) (*Defination, error) {
+	def, ok := definations[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op)
+	}
+	return def, nil
+}
+
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definations[op]
+	if !ok {
+		return []byte{}
+	}
+	instructionLen := 1
+	for _, w := range def.OperandWidths {
+		instructionLen += w
+	}
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	offset := 1
+	for i, o := range operands {
+		width := def.OperandWidths[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		}
+		offset += width
+	}
+	return instruction
+}
+
+func ReadOperands(def *Defination, ins Instructions) ([]int, int) {
+	operands := make([]int, len(def.OperandWidths))
+	offset := 0
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(ins[offset:]))
+		}
+		offset += width
+	}
+	return operands, offset
+}
+
+func ReadUint16(ins Instructions) uint16 {
+	return binary.BigEndian.Uint16(ins)
+}
+
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "Error: %s\n", err)
+			continue
+		}
+		operands, read := ReadOperands(def, ins[i+1:])
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+		i += 1 + read
+	}
+	return out.String()
+}
+
+func (ins Instructions) fmtInstruction(def *Defination, operands []int) string {
+	operandCount := len(def.OperandWidths)
+	if len(operands) != operandCount {
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n", len(operands), operandCount)
+	}
+	switch operandCount {
+	case 0:
+		return def.Name
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+	return fmt.Sprintf("Error: unhandled operandCount for %s\n", def.Name)
+}
